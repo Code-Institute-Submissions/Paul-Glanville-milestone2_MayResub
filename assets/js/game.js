8c5781f1e39961ba86f22a1b16af32adf34d3836
Paul-Glanville <glanville49@googlemail.com>
2021-01-30 13:40:52 +0000

    added starting contnet to game.js

---
+++ assets/js/game.js
@@ -0,0 +1,5 @@
+var boxArray = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen'];
+var box_value = [];
+var box_tile_ids = [];
+var tiles_saelected = 0;
+
