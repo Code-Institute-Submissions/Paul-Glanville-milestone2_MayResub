8c7329b0892f89506332204bb0bcc8b5ef7a07c8
Paul-Glanville <glanville49@googlemail.com>
2021-02-09 21:35:51 +0000

    added comments to game.js to help breakdown workload due to struggling on what to do and when

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,9 +1,11 @@
+/* Must declare each variable to begin with also as a reminder of what the variable names are*/
 var cardsArray  = [];
 var MemoryCounter = 0;
 var user = [];
-var tiles
+var tiles = []
 var start = document.getElementById(btn);
 
+/* Round update and new game */
 var updateRound = function(){
     rounds++;
     $("showRound").html(rounds);
@@ -21,6 +23,7 @@ function clearGame() {
     game.currentGame = [];
     game.count = 0;
 }
+/*Start Button*/
 $("#btn").on("click", function() {
     let ("start" == "refresh") {
         $("#btn").css("background-color", "red");
@@ -40,8 +43,14 @@ $("#btn").on("click", function() {
             setInterval(begin, 2000);}, 2000);
     }
 });
-
+/*Tiles and boxes within the grid, must create sequence for the,m to appear to the user */
 let tiles = document.getElementsByTagName("div").sibling("p");
 setTimeOut(function() {
-    console.log(cards)
+    console.log(tiles)
 }, 2000);
+
+/* user input to click on each box in order they are presented */
+
+/*boxes/tiles are to light up when sequence is run*/
+
+/*Boxes/tiles afre to light up when clicked by the user*/
