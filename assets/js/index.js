b9f4413ab81dd8f829bcb4eedf0a41604d5b5c56
Paul-Glanville <glanville49@googlemail.com>
2021-03-03 23:11:11 +0000

    added button id and start string to console log testing to check whether adjustement has correctly called and whether an error still appears

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,16 @@
-let start = true;
+let start = false;
 let cardsArray  = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 let memoryArray = [];
-let memoryCounter = 0;
+let memoryCounter = [];
 let playerArray = [];
-let playerCounter = 0;
-let levelCounter = 0;
+let playerCounter = [];
+let levelCounter = [];
 let level;
 
 $("#btn").on("click", function() {
-    start = begin;
+    start = true;
     $("#btn").addEventListener("begin").css("background-color", "red");
-        start = true;
+        start = "begin";
         playerArray = [];
         memoryArray = [];
         memoryCounter = 0;
@@ -21,5 +21,5 @@ $("#btn").on("click", function() {
         setTimeOut(function() {startMemory =
             setInterval(begin, 2000);}, 2000);
 });
-console.log(start, memoryArray, memoryCounter, clearInterval);
+console.log($("#btn"), start=true, memoryArray, memoryCounter, clearInterval);
 console.log(playerCounter, playerArray, levelCounter, cardsArray);
