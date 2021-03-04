8871f8b041520d7be593a0e29e88c484b2d7bf51
Paul-Glanville <glanville49@googlemail.com>
2021-03-04 14:59:26 +0000

    added setTimeout function for tiles and element tag to be called for the function

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,4 @@
-let start = false;
+let start = true;
 let cardsArray  = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 let memoryArray = [];
 let memoryCounter = [];
@@ -6,20 +6,34 @@ let playerArray = [];
 let playerCounter = [];
 let levelCounter = [];
 let level;
+let tiles;
+let startMemory;
 
 $("#btn").on("click", function() {
-    start = true;
+    start = "begin";
     $("#btn").addEventListener("begin").css("background-color", "red");
-        start = "begin";
-        playerArray = [];
-        memoryArray = [];
-        memoryCounter = 0;
-        playerCounter = 0;
-        levelCounter = 1;
+      playerArray = [];
+      memoryArray = [];
+      memoryCounter = 0;
+      playerCounter = 0;
+      levelCounter = 1;
     $(".box").html("event");
-        clearInterval(startMemory);
-        setTimeOut(function() {startMemory =
-            setInterval(begin, 2000);}, 2000);
+      clearInterval("begin");
+      setTimeout(function() {startMemory =
+          setInterval("begin", 2000);}, 2000);
+          return clearInterval;
 });
-console.log($("#btn"), start=true, memoryArray, memoryCounter, clearInterval);
+
+tiles = document.getElementsByTagName("p");
+setTimeout(function() {
+    levelCounter++;
+}, 2000);
+
+let game = {
+    count: 0,
+    currentGame: [],
+    player: [],
+};
+console.log($("#btn"), start, memoryArray, memoryCounter);
 console.log(playerCounter, playerArray, levelCounter, cardsArray);
+console.log(game, tiles);
