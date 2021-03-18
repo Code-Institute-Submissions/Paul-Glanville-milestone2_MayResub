927621584eca194013fdff20077c5a475ee0c9c6
Paul-Glanville <glanville49@googlemail.com>
2021-03-18 09:25:46 +0000

    added comments to identify more of what a function does, added event listener for tiles for the light up on click

--- assets/js/index.js
+++ assets/js/index.js
@@ -16,10 +16,10 @@ let startMemory;
 let matchArrays = true;
 
 
-let randomTileRange = (min, max) => {
+let randomTileRange = (min, max) => { /* this is the range in which the tiles are set and has a minimum and maximum */
 return Math.floor(Math.random() * (max - min) + min);
 }
-let randomTilesPlaces = (randomTilesNumber) => {
+let randomTilesPlaces = (randomTilesNumber) => { /* selects and lights up a random tile in a sequence*/
     let totalNumberOfTiles = 16;
     let randomTilesPlaces = [];
     while (randomTilesPlaces.length < randomTilesNumber) {
@@ -46,97 +46,41 @@ window.addEventListener('load', () => {
       tilePlaces.forEach((place, index) => {
       let tile = tiles[place - 1];
         if (tile) {
-            setTimeout(() => { 
+            setTimeout(() => { /* changes the colour of the tiles after a certain amount of time */
                 tile.style.backgroundColor = '#bee9e8';
             }, 0 + index * 500);
-            setTimeout(() => {
+            setTimeout(() => { /* reverts the tiles back to their original colour at end of sequence */
                 tile.style.backgroundColor = '#62b6cb';
             }, ((level+ 1) * 1000))
         }
       })
       level = level + 1; /* incrfease level by 1 */
     });
+});
 
-    /* whichToChange();
-    begin.addEventListener("click", () => {
-    if (tiles == true && true);
-        userCounter++;
+tiles.addEventListener("click", function(){
+    if (tile) {
+            lightUp(() => { /* changes the colour of the tiles after a certain amount of time */
+                tile.style.backgroundColor = '#bee9e8';
+            }, 0 + index * 500);
+            lightUp(() => { /* reverts the tiles back to their original colour at end of sequence */
+                tile.style.backgroundColor = '#62b6cb';
+            }, ((level+ 1) * 1000))
+        }
+});
 
+/*tiles.addEventListener("click", () => {
+    if (matchArrays == true && true)
+    console.log(matchArrays);
     for(let i = 0; i < userCounter.length; i ++){
         if (memory[i] != user[i]) {
-            matchingArrays = false;
-        }
-    }
-    if(matchArrays){
+            matchArrays = false;
+        }else{ matchArrays = true; {
         playerArray = [];
         memoryCounter = [];
         levelCounter = [];
-        newMemory = [];
-        /*setTimeout(function() {startMemory =
-            setInterval(start, 500);}, 500);
-    }*/
-});
-
-function numberChange(){
-  randomNum = Math.floor(Math.random() * 16 + 1);
-  let hashtag = "#";
-  return hashtag.concat(randomNum.toString());
-}
-
-function getColour(){
-  let colour = ["#bee9e8"];
-  randomNum = Math.floor(Math.random() * 4);
-  return(colour[randomNum]);
-}
-function whichToChange(){
-    number = numberChange();
-    colour = getColour();
-  $(number).css("background-color", colour);
-}
-
-function nextLevel() {
-    random = tiles[Math.floor(math.random) * number.length];
-    return random;
-}
-
-function show() {
-  hashtag = ["#"];
-  let h = 0;
-  for (h = 0; h <= hashtag.length; h++) {
-    if (h === hashtag.length) {
-      lightUp("run", h);
-    } else {
-      lightUp(hashtag[h], h);
+        newMemory = [];  
+        }
     }
-  }
-  console.log("Running show with: " + hashtag);
-  return hashtag;
-}
 
-function lightUp(name, number) {
-    setTimeout(function() {
-      if (name === number) {
-        playerTurn = true;
-      } else {
-        selected = name;
-      }
-    }, 500 * number);
-}
-    setTimeout(function() {
-    levelCounter++;
-}, 500);
-/* round update */
-let updateRound = function(){
-    rounds++;
-  $("showRounds").css("showRounds");
-};
-
-/* play round function 
-function playRound(nextSequence) {
-    nextSequence.forEach(function(number, index) {
-      setTimeout(function() {
-        tileActivate(number);
-      }, (index + 1) * 500);
-  });
-  return nextSequence;
-}*/
+});*/
